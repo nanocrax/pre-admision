0342dd17e828945b4a973c46a1669df144347e23
nanocrax <roldan.gabriel.sergio@gmail.com>
2020-08-07 11:24:11 -0300

    solucion problema de conexion con socket - agregacion de url

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:3000', { 'forceNew': true });
+var socket = io.connect('http://https://pre-admision.herokuapp.com/', { 'forceNew': true });
 
 //Pintamos los puestos y sus numeros
 socket.on('messages1', function(data1) {
